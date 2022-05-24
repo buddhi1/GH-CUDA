298434e968bb2089cc2156f218911d217ea6642a
buddhi1 <buddhi-ubuntu@LAPTOP-KLE3GLP3.localdomain>
2022-05-24 11:45:07 -0500

    intersection coding again

--- ghcuda.h
+++ ghcuda.h
@@ -2,3 +2,4 @@
 
 void testhello(void);
 void calculateIntersections(double *polyPX, double *polyPY, double *polyQX,  double *polyQY, int sizeP, int sizeQ);
+void countIntersections(double *polyPX, double *polyPY, double *polyQX,  double *polyQY, int sizeP, int sizeQ);
