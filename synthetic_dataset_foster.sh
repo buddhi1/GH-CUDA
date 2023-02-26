693425a1ead4a11ca93f7a154c373f6b383c8849
buddhi1 <buddhiashan8@gmail.com>
2023-02-25 18:20:44 -0600

    bashscript updated

--- synthetic_dataset_foster.sh
+++ synthetic_dataset_foster.sh
@@ -6,8 +6,8 @@ base=("lakes_851348" "s" "c" ${bDataset[0]}"500" ${bDataset[0]}"700" ${bDataset[
 overlay=("lakes-synthetic_1_1" "s-synthetic_15_10" "c-synthetic_100_0" ${oDataset[0]}"500" ${oDataset[0]}"700" ${oDataset[0]}"1000" ${oDataset[0]}"1500" ${oDataset[0]}"2000")
 
 # change following to select the test cases
-start=3
-end=5
+start=0
+end=7
 skip=10
 echo "Synthetic dataset execution Starting.."
 
